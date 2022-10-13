20e8dd944f400efae80496f60356c57778f73cfa
tomliumd <tom.liu@berkeley.edu>
2022-10-13 00:35:25 -0500

    echo

--- build/lib/echonet/datasets/__init__.py
+++ build/lib/echonet/datasets/__init__.py
@@ -4,5 +4,6 @@ echocardiogram videos.
 """
 
 from .echo import Echo
+from .echo import EchoAge
 
-__all__ = ("Echo", )
+__all__ = ("Echo", "Echo2")
--- echonet/datasets/__init__.py
+++ echonet/datasets/__init__.py
@@ -4,6 +4,6 @@ echocardiogram videos.
 """
 
 from .echo import Echo
-from .echo import Echo2
+from .echo import EchoAge
 
 __all__ = ("Echo", "Echo2")
