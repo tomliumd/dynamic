a150387d6eef17bb21b366f4b289c839aefa429d
tomliumd <tom.liu@berkeley.edu>
2022-12-04 16:40:18 -0600

    echoage

--- echonet/datasets/echo.py
+++ echonet/datasets/echo.py
@@ -414,7 +414,7 @@ class EchoAge(torchvision.datasets.VisionDataset):
 
                 self.header = data.columns.tolist()
                 self.fnames = data["FileName"].tolist()
-                self.fnames = [os.path.join(self.external_test_location, '_'.join(fn.split('/')[-2:])) + ".avi" for fn in
+                self.fnames = [os.path.join(self.external_test_location, fn.split('/')[-1]) + ".avi" for fn in
                                self.fnames if
                                os.path.splitext(fn)[1] == ""]  # Assume avi if no suffix
                 self.outcome = data.values.tolist()
@@ -448,7 +448,7 @@ class EchoAge(torchvision.datasets.VisionDataset):
 
             self.header = data.columns.tolist()
             self.fnames = data["FileName"].tolist()
-            self.fnames = [os.path.join(self.root, '_'.join(fn.split('/')[-2:])) + ".avi" for fn in
+            self.fnames = [os.path.join(self.root, fn.split('/')[-1]) + ".avi" for fn in
                                self.fnames if
                                os.path.splitext(fn)[1] == ""]
             # Assume avi if no suffix
